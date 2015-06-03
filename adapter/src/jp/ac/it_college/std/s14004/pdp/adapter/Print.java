9dfbe4b9859acead119e07ee75c70249f4eab159
s14004 <s14004@std.it-college.ac.jp>
2015-06-03 16:06:07 +0900

    二章おわり

---
+++ adapter/src/jp/ac/it_college/std/s14004/pdp/adapter/Print.java
@@ -0,0 +1,9 @@
+package jp.ac.it_college.std.s14004.pdp.adapter;
+
+/**
+ * Created by s14004 on 15/06/03.
+ */
+public interface Print {
+    public abstract void printWeek();
+    public abstract void printStrong();
+}
---
+++ adapter/src/jp/ac/it_college/std/s14004/pdp/adapter/PrintBanner.java
@@ -0,0 +1,20 @@
+package jp.ac.it_college.std.s14004.pdp.adapter;
+
+/**
+ * Created by s14004 on 15/06/03.
+ */
+public class PrintBanner extends Banner implements Print {
+    public PrintBanner(String string) {
+        super(string);
+    }
+
+    @Override
+    public void printWeek() {
+
+    }
+
+    @Override
+    public void printStrong() {
+
+    }
+}
